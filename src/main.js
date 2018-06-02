01934ec543cf5b7c5aa763f3248d9b11ee20e1ab
Alex Xin <xinba1989918@gmail.com>
2018-06-02 21:35:51 +1000

    remove unnecessary lib

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRangedatePicker from 'vue-rangedate-picker'
 
-Vue.use(VueRangedatePicker)
 new Vue({
   el: '#app',
   render: h => h(App)
